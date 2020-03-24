3ba8fd090291915eeae25a13a7e0e4a972b315f2
god <18762988789@163.com>
2020-03-24 16:32:22 +0800

    全是组件

--- vue/blank/pages/dPlay/dPlay.js
+++ vue/blank/pages/dPlay/dPlay.js
@@ -5,7 +5,18 @@ Page({
     data:{
       First:   app.globalData.foo,
       Second:  "scond",
-      true:    true
+      true:    true,
+      items:[
+        {name:'react',value:'REACT',checked:true},
+        {name:'vue', value:'VUE'}
+      ],
+      list:[
+        {id:0, name : '春'},
+        {id:1, name : '夏'},
+        {id:2, name : '秋'},
+        {id:3, name : '冬'}
+      ],
+      listIndex:0
     },
     onLoad(query){
       console.log("你好");
@@ -27,7 +38,11 @@ Page({
     viewTap(){},
 
     go(){
-      dd.navigateTo({url:"/pages/zx/zx"})
+      //dd.navigateTo({url:"/pages/zx/zx"})
+      dd.alert({
+        title:"傻猪猪"
+      }),
+      dd.stopPullDownRefresh()
     },
     retu(){
       dd.redirectTo({url:"/pages/vue/vue"})
@@ -35,5 +50,22 @@ Page({
   
   scrollupper(e){
     alert(e.detail);
+  },
+
+  onmit: function(e) {
+    console.log('form发生了submit事件，携带数据为：', e,e.detail.value)
+  },
+  onRe: function() {
+    console.log('form发生了reset事件')
+  },
+  radioGroupTap:function(e){
+    console.log("radioGroupTap"+e)
+  },
+
+  onchange(e){
+      console.log(e),
+      this.setData({
+          listIndex:e.detail.value
+      })
   }
 })
